6c7e6fa898d4f7d6419c9bf548a6eb6dc33133ba
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2024-09-26 23:07:40 +0530

    created users table

--- frontEnd/src/components/dashboard/Dashboard.jsx
+++ frontEnd/src/components/dashboard/Dashboard.jsx
@@ -1,32 +1,8 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { url } from "../../utils/url";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchAllUsers = async () => {
-      try {
-        const response = await axios.get(`${url}/admin/getUsers`, {
-          withCredentials: true,
-        });
-        setUsers(response.data.result);
-      } catch (err) {
-        if (err.response && err.response.status === 401) {
-          navigate("/unauthorized");
-          return;
-        }
-        if (err.response && err.response.status === 403) {
-          navigate("/forbidden");
-          return;
-        }
-        console.log(err);
-      }
-    };
-    fetchAllUsers();
-  }, [navigate]);
 
   return (
     <div>
--- frontEnd/src/components/users/Users.jsx
+++ frontEnd/src/components/users/Users.jsx
@@ -1,8 +1,57 @@
+import "./users.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { url } from "../../utils/url";
+import { useNavigate } from "react-router-dom";
+
 const Users = () => {
+  const [allUsers, setAllUsers] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      try {
+        const response = await axios.get(`${url}/admin/getUsers`, {
+          withCredentials: true,
+        });
+        setAllUsers(response.data.result);
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          navigate("/unauthorized");
+          return;
+        }
+        if (err.response && err.response.status === 403) {
+          navigate("/forbidden");
+          return;
+        }
+        console.log(err);
+      }
+    };
+    fetchAllUsers();
+  }, [navigate]);
+
   return (
-    <>
-      <div> this is users page </div>
-    </>
+    <div className="usersContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>ID </th>
+              <th> User Name</th>
+              <th> Email </th>
+              <th> Role </th>
+            </tr>
+          </thead>
+          <tbody>
+            {allUsers.map((user, idx) => (
+              <tr key={idx + 1}>
+                <td>{idx + 1} </td>
+                <td> {user.userName} </td>
+                <td> {user.email}</td>
+                <td> {user.role} </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </div>
   );
 };
 
