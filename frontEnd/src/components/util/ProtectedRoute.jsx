54198be25694f8628886c10500a0ff7a1e4baa01
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2024-09-27 09:50:32 +0530

    changed userRole to user for protected route

--- frontEnd/src/components/util/ProtectedRoute.jsx
+++ frontEnd/src/components/util/ProtectedRoute.jsx
@@ -1,6 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-export const ProtectedRoute = ({ allowedRoles, userRole }) => {
+export const ProtectedRoute = ({ allowedRoles, user }) => {
+  let userRole = null;
+  if (user) userRole = user.role;
+  else <Navigate to="/forbidden" />;
   if (allowedRoles.includes(userRole)) {
     return <Outlet />;
   } else {
